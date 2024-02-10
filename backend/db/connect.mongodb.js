ac6ac61c70ad22d03fc5a5007ad8161b406b6c57
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2024-02-10 17:45:03 +0530

    adding database

---
+++ backend/db/connect.mongodb.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+export const connectToMongoDB  =  async()=>{
+    try{
+      await mongoose.connect(process.env.DB_URL)
+      console.log('Connected to MongoDB')
+
+    }catch(err){
+        console.log(err)
+    }
+}
