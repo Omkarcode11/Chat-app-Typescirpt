bac8631c93c9438c6c1a775a0a5fda298a7f70b6
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2024-02-18 21:26:00 +0530

    create jwt authentication

--- backend/controllers/message.controllers.js
+++ backend/controllers/message.controllers.js
@@ -1,5 +1,6 @@
 import Conversation from "../model/Conversation.js";
 import Message from "../model/Message.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 import { ResponseClass } from "../utils/Classes.js";
 
 export const sendMessage = async (req, res) => {
@@ -13,30 +14,42 @@ export const sendMessage = async (req, res) => {
         $all: [senderId, receiverId],
       },
     });
+    
     if(!conversation){
         conversation = await Conversation.create({ participants:[senderId, receiverId] })
     }
 
     const newMessage = await Message.create({ senderId,receiverId,message })
-    
     if(newMessage){
         conversation.messages.push(newMessage._id)
         await conversation.save()
     }
 
-    res.status(200).send(new ResponseClass(newMessage,true))
+    res.status(200).send(new ResponseClass("success to send message",true,newMessage))
+
+    const receiverSocketId = getReceiverSocketId(receiverId)
+
+    if(receiverSocketId){
+      io.to(receiverSocketId).emit("newMessage", newMessage)
+    }
+
+
+
+
 
   } catch (err) {
     return res.status(500).send(new ResponseClass(err.message, false));
   }
+
+
+
 };
 
 
 export const getMessage = async(req,res)=>{
   try {
     const  receiverId  = req.params.id;
-    const senderId = String(req.user._id);
-    console.log(receiverId,senderId)
+    const senderId = req.user._id;
     let conversation =  await Conversation.findOne({
       participants: {
         $all: [senderId, receiverId] ,
@@ -44,12 +57,12 @@ export const getMessage = async(req,res)=>{
     }).populate({ path: 'messages', options: { strictPopulate: false } });
 
     if(!conversation){
-      return res.status(400).send(new ResponseClass('conversation not found',false)).exec()
+      return res.status(400).send(new ResponseClass('conversation not found',false))
     }
 
-    return res.status(200).send(new ResponseClass(conversation.messages,true))
+    return res.status(200).send(new ResponseClass("Getting Success",true,conversation.messages))
 
   }catch(err){
-    return res.status(500).send(new ResponseClass(err.message, false))
+    return res.status(500).send(new ResponseClass("server err from get message", false,err.message))
   }
 }
