6bcc8dd73d754f72526d927306d2ec80209e43f3
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2024-02-20 21:29:12 +0530

    modify generateToken

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 export const generateToken = (data,res)=>{
 
-   const token = jwt.sign({data},process.env.JWT_KEY,{expiresIn:"30d"})
+   const token = jwt.sign({data},process.env.JWT_KEY,{expiresIn:"1h"})
 
    res.cookie("jwt",token,{
     maxAge:15 * 24 *60 * 60 *100,
