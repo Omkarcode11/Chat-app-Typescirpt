581185b26e76af415f421a8d01e9d8cea871bcdc
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2024-02-21 21:33:41 +0530

    modify home componont

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,16 +1,34 @@
+import { useNavigate } from "react-router-dom";
 import MessageContainer from "../../components/message/MessageContainer";
 import NoChatSelected from "../../components/message/NoChatSelected";
 import SideBar from "../../components/sidebar/SideBar";
+import { useAuthContext } from "../../context/Authcontext";
+import { useEffect } from "react";
+import useConversationContext from "../../store/zustand";
 
 const Home: React.FC = () => {
-   let flag = true 
+  const {user}= useAuthContext()
+  const navigate = useNavigate()
+
+  const {selectedConversation,setSelectedConversation} = useConversationContext() 
+   
+   useEffect(()=>{
+    if(!user.id){
+       navigate("/signup")
+    }
+
+   },[])
+
+   useEffect(()=>{
+    return setSelectedConversation({...selectedConversation,_id :""} as any)
+   },[])
 
   return <div className="flex sm:h-[450px] md:h-[550] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
     <SideBar/>
-    {flag?
+    {!selectedConversation?._id.length?
     <NoChatSelected/>
     :
-    <MessageContainer/>
+    <MessageContainer firstName={selectedConversation.firstName} lastName={selectedConversation.lastName} profilePic={selectedConversation.profilePic} />
     }
   </div>;
 };
