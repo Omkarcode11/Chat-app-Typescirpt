1e7de034fc87e9433547a454be0d0950cb9485a8
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2024-02-21 21:31:58 +0530

    modify check box and signup

--- frontend/src/pages/signup/GenderCheckBox.tsx
+++ frontend/src/pages/signup/GenderCheckBox.tsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import React from "react";
 
-const  GenderCheckBox:React.FC  =() =>{
-  return (
-    <div className='flex'>
-        <div className='form-control'>
-         <label className="label gap-2 cursor-pointer">
-            <span className='label-text'>Male</span>
-            <input type='checkbox' className='checkbox checkbox-info border-slate-900'/>
-         </label>
-
-        </div>
-        <div className='form-control'>
-         <label className="label gap-2 cursor-pointer">
-            <span className='label-text'>Female</span>
-            <input type='checkbox' className='checkbox checkbox-secondary border-slate-900'/>
-         </label>
-
-        </div>
-
-
-    </div>
-  )
+interface GenderCh {
+  handleSelectGender: (gender: "male" | "female") => void;
+  val: "" | "male" | "female";
 }
 
-export default GenderCheckBox 
+const GenderCheckBox: React.FC<GenderCh> = ({ handleSelectGender ,val}) => {
+  return (
+    <div className='flex'>
+      <div className='form-control'>
+        <label className='label gap-2 cursor-pointer'>
+          <span className='label-text'>Male</span>
+          <input
+            type='checkbox'
+            className='checkbox checkbox-info border-slate-900'
+             checked={val=='male'}
+            value={"male"}
+            onChange={()=>handleSelectGender("male")}
+          />
+        </label>
+      </div>
+      <div className='form-control'>
+        <label className='label gap-2 cursor-pointer'>
+          <span className='label-text'>Female</span>
+          <input
+            type='checkbox'
+            className='checkbox checkbox-secondary border-slate-900'
+            checked={val=='female'}
+            onChange={()=>handleSelectGender("female")}
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default GenderCheckBox;
--- frontend/src/pages/signup/Signup.tsx
+++ frontend/src/pages/signup/Signup.tsx
@@ -1,6 +1,32 @@
+import { Link } from "react-router-dom";
 import GenderCheckBox from "./GenderCheckBox";
+import { useState } from "react";
+import { User } from "../../utils/Interfaces";
+import useSignUp from "../../hooks/useSignUp";
+
 
 const Signup: React.FC = () => {
+
+  const [user,setUser] = useState<User>({
+    firstName:"",
+    lastName:"",
+    email:"",
+    password:"",
+    gender:""
+  })
+  const {loading,signUp} = useSignUp()
+   
+  async function handleSubmit(e:React.FormEvent<HTMLFormElement>){
+    e.preventDefault()
+    await signUp(user)
+  }
+
+  function handleSelectGender(str:"male"|"female"){
+       setUser({...user,gender:str})
+       return;
+  }
+
+   
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
       <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -9,7 +35,7 @@ const Signup: React.FC = () => {
           <span className='text-blue-700'>Chatapp</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='flex flex-wrap'>
             <div className='mr-4'>
               <label className='label p-2'>
@@ -19,6 +45,8 @@ const Signup: React.FC = () => {
                 type='text'
                 className=' w-full input input-bordered h-10'
                 placeholder='Enter FirstName'
+                value={user.firstName}
+                onChange={(e)=>setUser({...user,firstName:e.target.value})}
               />
             </div>
             <div>
@@ -29,6 +57,8 @@ const Signup: React.FC = () => {
                 type='text'
                 className=' w-full input input-bordered h-10'
                 placeholder='Enter LastName'
+                value={user.lastName}
+                onChange={(e)=>setUser({...user,lastName:e.target.value})}
               />
             </div>
           </div>
@@ -41,30 +71,40 @@ const Signup: React.FC = () => {
               type='text'
               className=' w-full input input-bordered h-10'
               placeholder='Enter Email'
+              value={user.email}
+              onChange={(e)=>setUser({...user, email: e.target.value})}
             />
           </div>
 
           <div>
             <label className='label p-2'>
               <span className='text-base label-text'>Password</span>
+              
             </label>
             <input
               type='password'
               className=' w-full input input-bordered h-10'
-
               placeholder='Enter Password'
+              value={user.password}
+              onChange={(e)=>setUser({...user,password:e.target.value})}
             />
           </div>
-          <GenderCheckBox />
+          <GenderCheckBox handleSelectGender={handleSelectGender} val={user.gender} />
 
-          <a
-            href='#'
+          <Link
+            to='/login'
             className='text-sm hover:underline hover:text-blue-900 mt-2 inline-block'>
             Already have account?
-          </a>
+          </Link>
 
           <div>
-            <button className='btn btn-block btn-sm mt-2'>Signup</button>
+            <button  type="submit" className='btn btn-block btn-sm mt-2'>
+              {loading?
+               <span className="loading loading-spinner"></span>:
+                "SignUp"
+              }
+              
+            </button>
           </div>
         </form>
       </div>
