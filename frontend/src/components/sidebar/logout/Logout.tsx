3b95909cfeb44bdc7528505d76d87428ff0200ae
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2024-02-22 21:41:10 +0530

    adding functionality on logout

--- frontend/src/components/sidebar/logout/Logout.tsx
+++ frontend/src/components/sidebar/logout/Logout.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { SlLogout } from "react-icons/sl";
+import useLogout from '../../../hooks/useLogout';
 
 const Logout:React.FC=()=> {
+  let {logOut} = useLogout()
+
+   function handleLogout():void{
+    logOut()
+   }
+   
+
   return (
     <>
       <div className="divider p-0 m-0 h-1"></div>
 
-    <div className='mt-auto'><SlLogout className='w-6 h-6 text-white cursor-pointer' /></div>
+    <div className='mt-auto'><SlLogout onClick={handleLogout}  className='w-6 h-6 text-white cursor-pointer' /></div>
     </>
   )
 }
