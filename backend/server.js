2e28d8db93abe75de8d74e5f8394ea7108552a23
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2024-02-25 18:26:59 +0530

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from "path"
 import dotenv from 'dotenv'
 import { connectToMongoDB } from './db/connect.mongodb.js'
 import cookieParser from 'cookie-parser'
@@ -9,12 +10,20 @@ import {app,server} from './socket/socket.js'
 
 const PORT = process.env.PORT || 8080;
 
+const __dirname = path.resolve()
+
 dotenv.config()
 app.use(cors({ credentials: true, origin: true }))
 app.use(cookieParser())
 app.use(express.json())
 
 app.use(router)
+
+
+app.use(express.static(path.join(__dirname,'/frontend/dist')))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,',frontend' ,'dist','index.html'))
+})
 
 
 server.listen(PORT, async()=>{
