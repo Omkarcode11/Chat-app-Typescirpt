abc79665d4dae8b32261786c58f84d5154bc25b1
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2024-02-25 22:15:07 +0530

    add __dirname from path module

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import {app,server} from './socket/socket.js'
 
 const PORT = process.env.PORT || 8080;
 
-// const __dirname = path.resolve()
+const __dirname = path.resolve()
 
 dotenv.config()
 app.use(cors({ credentials: true, origin: true }))
