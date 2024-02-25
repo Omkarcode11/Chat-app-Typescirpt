3dcc4e3f49b4b25ac6b064b1cf280504c1fc864f
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2024-02-25 23:02:51 +0530

    fix styles

--- frontend/src/components/message/Message.tsx
+++ frontend/src/components/message/Message.tsx
@@ -11,7 +11,7 @@ const Message: React.FC<messageComponentProps> = ({side,message,time}) => {
 
     
   return (
-    <div className={`chat chat-${side=='end'?"end":'start'}`}>
+    <div className={`chat ${side=='end'?"chat-end":'chat-start'}`}>
       <div className={`chat-bubble text-white ${side=='end'?"bg-blue-500":"bg-gray-700"}`}>{message}</div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">{time}</div>
     </div>
